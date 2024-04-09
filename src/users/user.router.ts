a3d29cf70dbcda3ff95d5ee1a379349ce1f75fdd
José Ramírez <jota.rf.88@gmail.com>
2024-04-09 11:03:48 -0700

    Add user router for handling requests and use of validation middlewares

--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -1,8 +1,19 @@
 import Router from 'express'
-import { Request, Response } from 'express'
+//import { HTTP_STATUS } from '../common/constants/http-codes.constants'
+import { validateParamsMiddleware } from './middlewares/validate-params.middleware'
+import { validateUserMiddleware } from './middlewares/validate-user.middleware'
+import { userController } from './user.controller'
 
 export const userRouter = Router()
 
-userRouter.get('/', (_: Request, res: Response) => {
-  res.send('you are getting a user...')
-})
+userRouter.post('/', validateUserMiddleware, userController.createUser)
+userRouter.get('/', userController.getAllUsers)
+userRouter.get('/id/:userId', validateParamsMiddleware, userController.getUserById)
+userRouter.get('/email/:email', validateParamsMiddleware, userController.getUserByEmail)
+userRouter.put(
+  '/:userId',
+  validateParamsMiddleware,
+  validateUserMiddleware,
+  userController.updateUser
+)
+userRouter.delete('/:userId', validateParamsMiddleware, userController.deleteUser)

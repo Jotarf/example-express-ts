fdc95d39c16a9250ff94050acea825b6fc2dab0d
José Ramírez <jota.rf.88@gmail.com>
2024-04-13 17:34:13 -0400

    Rename validate user to validate create user

---
+++ src/users/middlewares/validate-create-user.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express'
+import { HTTP_STATUS } from '../../common/constants/http-codes.constants'
+import { createUserSchemaValidation } from '../validations/user.validation'
+import { CreateUserDTO } from '../dtos/create-user.dto'
+
+export function validateCreateUserMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const user: CreateUserDTO = {
+    fullname: req.body.fullname,
+    email: req.body.email
+  }
+
+  const { error } = createUserSchemaValidation.validate(user)
+
+  if (error)
+    return res.status(HTTP_STATUS.BAD_REQUEST).json({ error: error.details[0].message })
+
+  next()
+}

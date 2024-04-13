49600dac8132fee9f045943a07382dfd1bc1494d
José Ramírez <jota.rf.88@gmail.com>
2024-04-13 17:36:42 -0400

    Refactor of user validation schema to use isolated user fields

--- src/users/validations/user.validation.ts
+++ src/users/validations/user.validation.ts
@@ -2,14 +2,18 @@ import { SchemaValidation } from '../../common/types/schema-validation.type'
 import { CreateUserDTO } from '../dtos/create-user.dto'
 import { UserDTO } from '../dtos/user.dto'
 import { userSchema } from '../schemas/user.schema'
+import joi from 'joi'
 
-export const createUserSchemaValidation: SchemaValidation<CreateUserDTO> = {
-  fullname: userSchema.userFullnameSchema.required(),
-  email: userSchema.userEmailSchema.required()
+const createUserSchema: SchemaValidation<CreateUserDTO> = {
+  fullname: userSchema.fullnameField,
+  email: userSchema.emailField
 }
 
-export const updateUserSchemaValidation: SchemaValidation<UserDTO> = {
-  fullname: userSchema.userFullnameSchema.required(),
-  email: userSchema.userEmailSchema.required(),
-  id: userSchema.userIdSchema.required()
+const updateUserSchema: SchemaValidation<UserDTO> = {
+  fullname: userSchema.fullnameField,
+  email: userSchema.emailField,
+  id: userSchema.idField
 }
+
+export const createUserSchemaValidation = joi.object(createUserSchema)
+export const updateUserSchemaValidation = joi.object(updateUserSchema)

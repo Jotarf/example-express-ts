1bf93b1ef1f2a4189b0e816bce82612c57dee252
José Ramírez <jota.rf.88@gmail.com>
2024-04-13 16:28:40 -0400

    Separate creater user and update user validations into their own schemas

--- src/users/validations/user.validation.ts
+++ src/users/validations/user.validation.ts
@@ -1,9 +1,15 @@
+import { SchemaValidation } from '../../common/types/schema-validation.type'
+import { CreateUserDTO } from '../dtos/create-user.dto'
+import { UserDTO } from '../dtos/user.dto'
 import { userSchema } from '../schemas/user.schema'
 
-const userSchemaToValidate = userSchema.userEmailSchema.concat(
-  userSchema.userFullnameSchema
-)
+export const createUserSchemaValidation: SchemaValidation<CreateUserDTO> = {
+  fullname: userSchema.userFullnameSchema.required(),
+  email: userSchema.userEmailSchema.required()
+}
 
-export const userSchemaValidation = {
-  user: userSchemaToValidate.required()
+export const updateUserSchemaValidation: SchemaValidation<UserDTO> = {
+  fullname: userSchema.userFullnameSchema.required(),
+  email: userSchema.userEmailSchema.required(),
+  id: userSchema.userIdSchema.required()
 }

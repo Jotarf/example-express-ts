fecdbb7a2ce9848c623964a46c93a9a79a9828f9
José Ramírez <jota.rf.88@gmail.com>
2024-04-15 12:23:02 -0400

    Update user in database and improve errors

--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -1,3 +1,4 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { RepositoryResultDTO } from '../common/constants/dtos/repository-result.dto'
 import { getPrismaClient } from '../common/prisma/prisma.config'
 import { CreateUserDTO } from './dtos/create-user.dto'
@@ -86,22 +87,30 @@ const updateUser = async (
   updatedUser: UserDTO
 ): Promise<RepositoryResultDTO<null>> => {
   try {
-    const result: RepositoryResultDTO<null> = await new Promise((resolve, reject) => {
-      const user: UserDTO | undefined = users.find((user) => user.id === userId)
-
-      if (!user) reject({ error: true, message: 'User not found' })
-
-      users = users.map((user) => (user.id === userId ? updatedUser : user))
-
-      resolve({ error: false })
+    await prismaClient.user.update({
+      data: {
+        email: updatedUser.email,
+        fullname: updatedUser.fullname
+      },
+      where: {
+        id: userId
+      }
     })
+
+    const result: RepositoryResultDTO<null> = { error: false }
 
     return result
   } catch (error: unknown) {
-    return {
-      error: true,
-      message: (error as RepositoryResultDTO<null>)?.message ?? "User couldn't be updated"
-    }
+    if (error instanceof PrismaClientKnownRequestError)
+      return {
+        error: true,
+        message: error?.meta?.cause as string
+      }
+    else
+      return {
+        error: true,
+        message: "User couldn't be updated"
+      }
   }
 }
 

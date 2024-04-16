77cca55694b396e58cb3731ca9518f1414540c34
José Ramírez <jota.rf.88@gmail.com>
2024-04-16 17:37:44 -0400

    Add authentication router and routes for user authentication

--- src/main.router.ts
+++ src/main.router.ts
@@ -1,6 +1,8 @@
 import Router from 'express'
 import { userRouter } from './users/user.router'
+import { authRouter } from './auth/auth.router'
 
 export const router = Router()
 
 router.use('/users', userRouter)
+router.use('/auth', authRouter)

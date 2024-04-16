9e29627bee2286ad880a3d0cfdd6715e2ed1c916
José Ramírez <jota.rf.88@gmail.com>
2024-04-16 17:46:33 -0400

    Implement cookie parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { router } from './main.router'
+import cookieParser from 'cookie-parser'
 
 export const app = express()
 const globalPrefix = '/api'
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(globalPrefix, router)

0ecf5246fe0f57b30c309f74b9dff95f7c983472
José Ramírez <jota.rf.88@gmail.com>
2024-04-16 17:35:48 -0400

    Add util to compare password against hash

--- src/common/utils/hash.util.ts
+++ src/common/utils/hash.util.ts
@@ -4,3 +4,8 @@ export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10)
   return bcrypt.hash(password, salt)
 }
+
+export const compareHash = async (password: string, hash: string): Promise<boolean> => {
+  const arePasswordsEqual = await bcrypt.compare(password, hash)
+  return arePasswordsEqual
+}

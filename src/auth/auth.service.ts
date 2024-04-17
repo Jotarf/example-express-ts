4d10e1148b28da4abc30655589a4a007ebbfe479
José Ramírez <jota.rf.88@gmail.com>
2024-04-17 13:30:03 -0400

    Return token from auth repository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { RepositoryResultDTO } from '../common/constants/dtos/repository-result.dto'
+import { UserDTO } from '../users/dtos/user.dto'
 import { authRepository } from './auth.repository'
 import { LoginDTO } from './dtos/login.dto'
 
 const login = async (
   loginCredentials: LoginDTO
-): Promise<RepositoryResultDTO<{ token: string }>> => {
+): Promise<RepositoryResultDTO<{ user: UserDTO; token: string }>> => {
   try {
     const result = await authRepository.login(loginCredentials)
     return result
   } catch (error: unknown) {
-    return error as RepositoryResultDTO<{ token: string }>
+    return error as RepositoryResultDTO<{ user: UserDTO; token: string }>
   }
 }
 

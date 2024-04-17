f39146018937c01825e6cef0f1a79081bedbb526
José Ramírez <jota.rf.88@gmail.com>
2024-04-17 14:01:17 -0400

    Add e2e tests for user authentication

---
+++ test/e2e/auth/auth.test.ts
@@ -0,0 +1,50 @@
+import { LoginDTO } from '../../../src/auth/dtos/login.dto'
+import { HTTP_STATUS } from '../../../src/common/constants/http-codes.constants'
+import { UserDTO } from '../../../src/users/dtos/user.dto'
+import { userService } from '../../../src/users/user.service'
+import { api, getAllUsers, prismaClient, usersToCreate } from '../users/users.util'
+import * as jwt from 'jsonwebtoken'
+
+beforeAll(async () => {
+  const users: UserDTO[] = (await getAllUsers()).body
+
+  for (const user of users) await userService.deleteUser(user.id)
+
+  await prismaClient.$queryRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`
+
+  await userService.createUser(usersToCreate[0])
+})
+
+describe('Auth User', () => {
+  test('Should authenticate user', async () => {
+    const users = (await getAllUsers()).body
+
+    const loginCredentials: LoginDTO = {
+      email: usersToCreate[0].email,
+      password: usersToCreate[0].password
+    }
+
+    const response = await api
+      .post('/api/auth')
+      .send(loginCredentials)
+      .expect(HTTP_STATUS.OK)
+
+    expect(response.body).toEqual(users[0])
+    expect(response.headers['set-cookie']).toBeDefined()
+
+    const jwtCookie = response.headers['set-cookie'][0]
+      .split(';')
+      .find((cookie: string) => cookie.startsWith('jwt='))
+    const token = jwtCookie!.split('=')[1]
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string)
+
+    expect(decodedToken).toBeDefined()
+  })
+})
+
+afterAll(async () => {
+  const users: UserDTO[] = (await getAllUsers()).body
+
+  for (const user of users) await userService.deleteUser(user.id)
+  await prismaClient.$disconnect()
+})

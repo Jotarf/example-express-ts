5885e03c521ab830bd1726d959abc82f3740db4e
José Ramírez <jota.rf.88@gmail.com>
2024-04-17 15:34:35 -0400

    Add jwt validation middleware to get user by id endpoint

--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -3,12 +3,18 @@ import { validateParamsMiddleware } from './middlewares/validate-params.middlewa
 import { validateCreateUserMiddleware } from './middlewares/validate-create-user.middleware'
 import { userController } from './user.controller'
 import { validateUserMiddleware } from './middlewares/validate-user.middleware'
+import { validateJWTMiddleware } from '../auth/middlewares/jwt.middleware'
 
 export const userRouter = Router()
 
 userRouter.post('/', validateCreateUserMiddleware, userController.createUser)
 userRouter.get('/', userController.getAllUsers)
-userRouter.get('/id/:userId', validateParamsMiddleware, userController.getUserById)
+userRouter.get(
+  '/id/:userId',
+  validateParamsMiddleware,
+  validateJWTMiddleware,
+  userController.getUserById
+)
 userRouter.get('/email/:email', validateParamsMiddleware, userController.getUserByEmail)
 userRouter.put(
   '/:userId',

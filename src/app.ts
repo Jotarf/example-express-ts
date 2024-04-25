e07419c3a8fbe546a4cc21102b87f4df85f01b62
José Ramírez <jota.rf.88@gmail.com>
2024-04-25 11:28:44 -0400

    Import swagger middleware and serve it when it is not test enviroment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { router } from './main.router'
 import cookieParser from 'cookie-parser'
+import { swagger } from './common/swagger'
 
 export const app = express()
 const globalPrefix = '/api'
@@ -8,3 +9,5 @@ const globalPrefix = '/api'
 app.use(cookieParser())
 app.use(express.json())
 app.use(globalPrefix, router)
+
+if (process.env.NODE_ENV !== 'test') swagger(app)
